336563f916b7438988ee77873f8927c3d7b68f59
FireWizard312 <47069075+FireWizard312@users.noreply.github.com>
2020-12-30 17:10:21 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import base64
+
+def getatoken():
+    clientid = "b3186d148d7943298d295f09f6c264f8"
+    clientsec = "919a4d95eada4419af540dfb5e53d82c"
+    basic = base64.b64encode(clientid + ":" + clientsec)
+    rtoken = 'AQDFyYwEATPtg6Q2XuF_Q86Tx8LYWXuj-fkTyrMeJ_nb8VP8UwDpkAxEC1cJ6Yu4xYt5HoLOVANoVgalq366PvAtXZhfxWTp3g5VsTeoZ4B4rofKg3Wo8ZbJuK1gQB5GgdI'
+    theaders = {'Authorization': 'basic ' + basic}
+    atoken = requests.post('https://accounts.spotify.com/api/token', headers=theaders, data={'grant_type': 'refresh_token', 'refresh_token': rtoken})
+    atokenstr = json.loads(atoken.content)
+    accesstoken = atokenstr['access_token']
+    headers = {'Accept': 'application/json', 'Content-Type':'application/json', 'Authorization': 'Bearer ' + accesstoken}
+
+def volumeup():
+    info = requests.get('https://api.spotify.com/v1/me/player', headers=headers)
+    infostr = json.loads(info.content)
+    volmeper = int(infostr['device']['volume_percent'])
+    volup = requests.put('https://api.spotify.com/v1/me/player/volume?volume_percent=' + str(volmeper + 10), headers=headers
+
+def volumedown():
+    info = requests.get('https://api.spotify.com/v1/me/player', headers=headers)
+    infostr = json.loads(info.content)
+    volmeper = int(infostr['device']['volume_percent'])
+    voldw = requests.put('https://api.spotify.com/v1/me/player/volume?volume_percent=' + str(volmeper - 10), headers=headers)
+
+def pausemusic():
+    pause = requests.put('https://api.spotify.com/v1/me/player/pause', headers=headers)
+
+def playmusic():
+    play = requests.put('https://api.spotify.com/v1/me/player/play', headers=headers)
+
+def previous():
+    prev = requests.post('https://api.spotify.com/v1/me/player/previous', headers=headers)
+
+def nextsong():
+    next = requests.post('https://api.spotify.com/v1/me/player/next', headers=headers)
+
